7b1df19d84f16b097961cc3ad7020c5a1d3028c8
likeSerge <likeserge@gmail.com>
2018-03-05 07:55:48 +0200

    webpack-typescript

---
+++ wp_ts/src/index.ts
@@ -0,0 +1,5 @@
+console.log(test());
+
+function test(): string {
+  return 'TS compiled!!!';
+}
---
+++ wp_ts/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  devServer: {
+    contentBase: './dist'
+  },
+
+  resolve: {
+    extensions: [".js", ".ts"]
+  },
+
+  module: {
+    rules: [
+      {test: /\.ts$/, loader: "ts-loader"},
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ],
+
+  mode: "development"
+};
